7136c6e74d03b2c09d9f9386abe57c2b5136bb8c
Konstantin Polygalov <sailorcast@mail.ru>
2017-10-18 20:25:23 +0300

    some changes

--- app/src/main/java/com/polygalov/gpsappwidget/GPSAppWidgetProvider.java
+++ app/src/main/java/com/polygalov/gpsappwidget/GPSAppWidgetProvider.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 public class GPSAppWidgetProvider extends AppWidgetProvider {
 
-
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
